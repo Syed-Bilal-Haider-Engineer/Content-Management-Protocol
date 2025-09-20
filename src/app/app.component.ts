299dd5a24c138f43e9b827e06d2e780fcf33bf2d
syed bilal haider <bilaldev151214@gmail.com>
2025-09-20 21:45:15 +0200

    Reusable component for content dashboard item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeadersComponent } from "./headers/headers.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeadersComponent, TicketsComponent, ServerStatusComponent, TrafficComponent],
+  imports: [HeadersComponent, TicketsComponent, ServerStatusComponent, TrafficComponent, DashboardItemComponent],
 })
 export class AppComponent {
 }
