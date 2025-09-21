82611f7d82f22f56365390ccfef0550c82bed7fe
syed bilal haider <bilaldev151214@gmail.com>
2025-09-21 22:38:24 +0200

    Added host selector  and removed unnecessary element.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
