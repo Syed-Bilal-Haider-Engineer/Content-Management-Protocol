cc90b715992cc5d059d4c8cc26469f968f220e10
syed bilal haider <bilaldev151214@gmail.com>
2025-09-25 23:24:16 +0200

    Added life cycle and update server status

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,13 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrls: ['./server-status.component.css']
 })
-export class ServerStatusComponent {
-    currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  currentStatus = 'online';
+  private intervalId: any;
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      const rnd = Math.floor(Math.random() * 10)
+
+      if (rnd > 5 && rnd <= 8) {
+        this.currentStatus = 'offline';
+      } else if (rnd > 8 && rnd <= 10) {
+        this.currentStatus = 'away';
+      } else {
+        this.currentStatus = 'online';
+      }
+    }, 3000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
